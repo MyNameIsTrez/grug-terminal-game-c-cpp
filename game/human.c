8551d846ef54fac3aae9398fe7f073eaf52d8b8e
MyNameIsTrez <welfje@gmail.com>
2024-11-21 19:38:59 +0100

    Add `clamp_i32()`

--- game/human.c
+++ game/human.c
@@ -11,23 +11,19 @@ id game_fn_get_opponent(id human_id) {
 }
 
 static i32 min_i32(i32 a, i32 b) {
-	if (a < b) {
-		return a;
-	}
-	return b;
+	return a < b ? a : b;
 }
 
 static i32 max_i32(i32 a, i32 b) {
-	if (a > b) {
-		return a;
-	}
-	return b;
+	return a > b ? a : b;
+}
+
+static i32 clamp_i32(i32 n, i32 lowest, i32 highest) {
+	return max_i32(lowest, min_i32(highest, n));
 }
 
 void game_fn_change_human_health(id id, i32 added_health) {
 	assert(id < 2);
 	human *h = &data.humans[id];
-
-	h->health = min_i32(h->health + added_health, h->max_health);
-	h->health = max_i32(h->health, 0);
+	h->health = clamp_i32(h->health + added_health, 0, h->max_health);
 }
