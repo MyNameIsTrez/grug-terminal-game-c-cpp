691a48bbe0c3ffdf056be5eee44f46d3d1ec5658
MyNameIsTrez <welfje@gmail.com>
2024-12-02 20:15:22 +0100

    Print invalid game fn arguments

--- game/human.c
+++ game/human.c
@@ -4,9 +4,13 @@
 #include "typedefs.h"
 
 #include <assert.h>
+#include <stdio.h>
 
 id game_fn_get_opponent(id human_id) {
-	assert(human_id < 2);
+	if (human_id >= 2) {
+		fprintf(stderr, "grug runtime error in %s(): the human_id argument of get_opponent() was %zu, while the function only expects it to be up to 2, in %s\n", grug_on_fn_name, human_id, grug_on_fn_path);
+		return -1;
+	}
 	return data.humans[human_id].opponent_id;
 }
 
@@ -22,8 +26,15 @@ static i32 clamp_i32(i32 n, i32 lowest, i32 highest) {
 	return max_i32(lowest, min_i32(highest, n));
 }
 
-void game_fn_change_human_health(id id, i32 added_health) {
-	assert(id < 2);
-	human *h = &data.humans[id];
+void game_fn_change_human_health(id human_id, i32 added_health) {
+	if (human_id >= 2) {
+		fprintf(stderr, "grug runtime error in %s(): the human_id argument of change_human_health() was %zu, while the function only expects it to be up to 2, in %s\n", grug_on_fn_name, human_id, grug_on_fn_path);
+		return;
+	}
+	if (added_health == -42) {
+		fprintf(stderr, "grug runtime error in %s(): the added_health argument of change_human_health() was -42, while the function deems that number to be forbidden, in %s\n", grug_on_fn_name, grug_on_fn_path);
+		return;
+	}
+	human *h = &data.humans[human_id];
 	h->health = clamp_i32(h->health + added_health, 0, h->max_health);
 }
--- game/tool.c
+++ game/tool.c
@@ -4,8 +4,12 @@
 #include "typedefs.h"
 
 #include <assert.h>
+#include <stdio.h>
 
 id game_fn_get_human_parent(id tool_id) {
-	assert(tool_id < 2);
+	if (tool_id >= 2) {
+		fprintf(stderr, "grug runtime error in %s(): the tool_id argument of get_human_parent() was %zu, while the function only expects it to be up to 2, in %s\n", grug_on_fn_name, tool_id, grug_on_fn_path);
+		return -1;
+	}
 	return data.tools[tool_id].human_parent_id;
 }
